875889e06eeb5e5117ffb1f6da14756d632c0219
Ievgen Borodin <ievgenborodin@gmail.com>
2015-12-03 20:48:38 -0500

    Github links add, commented functions app

--- js/controllers.js
+++ js/controllers.js
@@ -32,8 +32,10 @@ angular.module('my.portfolio.controller', [])
          name: 'Bandshell Booking',
          imgSrc: 'bandshell.jpg',
          info: 'A show booking and promotions agency startup website with responsive design.',
+         infoSpan: 'Additional [heroku-rails-postgresql] version link on github > rails branch.',
          infoLangs: 'php, laravel, mysql, javascript',
          href: 'http://bandshell.ly/',
+         gitLink: 'https://github.com/ievgenborodin/bandshell',
          color: "#0101ff",
          mobilePhoto: 'bandshell/2.gif',  
          desktopPhoto: 'bandshell/1.jpg'
@@ -44,6 +46,7 @@ angular.module('my.portfolio.controller', [])
          info: '2d arcade game with responsive design and keyboard controls for laptop / desktop computers and virtual joystick for mobile devices.',
          infoLangs: 'html5-canvas, javascript',
          href: 'apps/raccoon',
+         gitLink: 'https://github.com/ievgenborodin/raccoon',
          color: "#158e09",
          mobilePhoto: 'raccoon/2.gif',
          desktopPhoto: 'raccoon/1.jpg'  
@@ -54,6 +57,7 @@ angular.module('my.portfolio.controller', [])
          info: 'Out of curiosity. Implementation of matrix for Diamonds. Supports touch events and mouse.',
          infoLangs: 'html5-canvas, javascript',
          href: 'apps/gems',
+         gitLink: 'https://github.com/ievgenborodin/gems',
          color: "#ff6a00",
          mobilePhoto: 'gems/2.jpg',
          desktopPhoto: 'gems/1.jpg'
@@ -64,6 +68,7 @@ angular.module('my.portfolio.controller', [])
          info: 'Browser oriented hybrid graphical app. Combines raster and object based drawing. Supports mouse and touch events.',
          infoLangs: 'html5-canvas, javascript, php',
          href: 'apps/livebrush',
+         gitLink: 'https://github.com/ievgenborodin/live-brush',
          color: "#098e60",
          mobilePhoto: 'brush/2.gif',
          desktopPhoto: 'brush/1.jpg'
@@ -74,6 +79,7 @@ angular.module('my.portfolio.controller', [])
          info: 'Select color using hue bar and saturation-value field or by entering hash code. Bottom block gives ten random colors. Supports mouse and touch events.',
          infoLangs: 'javascript',
          href: 'apps/colorpicker',
+         gitLink: 'https://github.com/ievgenborodin/color-picker',
          color: "#ac22cc",
          mobilePhoto: 'picker/2.gif',
          desktopPhoto: 'picker/1.jpg'
@@ -84,6 +90,7 @@ angular.module('my.portfolio.controller', [])
          info: 'Image depth generator. Searches through the image for outlines, based on found colors builds depth. Use sample images or load your pictures. Supports mouse and touch events.',
          infoLangs: 'html5-canvas, javascript, php',
          href: 'apps/tri',
+         gitLink: 'https://github.com/ievgenborodin/tri',
          color: "#299e56",
          mobilePhoto: 'tri/2.gif',
          desktopPhoto: 'tri/1.jpg'
@@ -94,6 +101,7 @@ angular.module('my.portfolio.controller', [])
          info: 'Scalable background pattern tool. Helps to draw a grid / tile images.',
          infoLangs: 'javascript, php',
          href: 'apps/backgrid',
+         gitLink: 'https://github.com/ievgenborodin/backgrid',
          color: "#5e2400",
          mobilePhoto: 'grid/2.gif',
          desktopPhoto: 'grid/1.jpg'
@@ -104,6 +112,7 @@ angular.module('my.portfolio.controller', [])
          info: 'Web application. Structured as a mini platform for implementation and testing geometric animation.',
          infoLangs: 'html5-canvas, javascript, jquery, requirejs',
          href: 'apps/lines',
+         gitLink: 'https://github.com/ievgenborodin/lines',
          color: "#211b80",
          mobilePhoto: 'lines/2.gif',
          desktopPhoto: 'lines/1.jpg'
@@ -114,6 +123,7 @@ angular.module('my.portfolio.controller', [])
          info: 'The idea is to learn how to draw with the help of built-in tools. Filter that takes away extra elements out of images. Custom grid splits photo into sections. Colors schemas.',
          infoLangs: 'html5-canvas, javascript',
          href: 'apps/dro',
+         gitLink: 'https://github.com/ievgenborodin/dro',
          color: "#4a5077",
          mobilePhoto: 'dro/2.gif',
          desktopPhoto: 'dro/1.jpg'
@@ -124,18 +134,20 @@ angular.module('my.portfolio.controller', [])
          info: 'Base for 2d with side view arcade game. Supports keyboard and touches.',
          infoLangs: 'html-canvas, javascript',
          href: 'apps/monk',
+         gitLink: 'https://github.com/ievgenborodin/monk',
          color: "#ad490c",
          desktopPhoto: 'monk/1.jpg'
-      },
+      }/*,
       {
          name: 'Functions',
          imgSrc: 'functions.jpg',
          info: 'Functions and their graphs.',
          infoLangs: 'html-canvas, javascript',
          href: 'apps/functions',
+         gitLink: 'https://github.com/ievgenborodin/function',
          color: "#8e0000",
          desktopPhoto: 'functions/1.jpg'
-      }
+      }*/
     ];
     
     /* footer my contacts */
